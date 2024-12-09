df7f25df89f18705a6a87a64e485a4323932504c
benadelberg <bennoahadelberg@gmail.com>
2024-12-09 23:28:07 +0000

    first commit

---
+++ create_tables.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+conn = sqlite3.connect("grocery_store.db")
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS Suppliers")
+cursor.execute("DROP TABLE IF EXISTS Customers")
+cursor.execute("DROP TABLE IF EXISTS Products")
+cursor.execute("DROP TABLE IF EXISTS Orders")
+cursor.execute("DROP TABLE IF EXISTS OrderDetails")
+
+cursor.execute("""
+CREATE TABLE Products (
+    ProductID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ProductName TEXT NOT NULL,
+    Category TEXT,
+    Price REAL NOT NULL,
+    StockQuantity INTEGER NOT NULL,
+    SupplierID INTEGER,
+    FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID)
+)
+""")
+
+cursor.execute("""
+CREATE TABLE Customers (
+    CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,
+    FirstName TEXT NOT NULL,
+    LastName TEXT NOT NULL,
+    Email TEXT UNIQUE,
+    LoyaltyPoints INTEGER DEFAULT 0
+)
+""")
+
+cursor.execute("""
+CREATE TABLE Orders (
+    OrderID INTEGER PRIMARY KEY AUTOINCREMENT,
+    CustomerID INTEGER,
+    OrderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    TotalAmount REAL NOT NULL,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+)
+""")
+
+cursor.execute("""
+CREATE TABLE OrderDetails (
+    OrderDetailID INTEGER PRIMARY KEY AUTOINCREMENT,
+    OrderID INTEGER,
+    ProductID INTEGER,
+    Quantity INTEGER NOT NULL,
+    Price REAL NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+)
+""")
+
+cursor.execute("""
+CREATE TABLE Suppliers (
+    SupplierID INTEGER PRIMARY KEY AUTOINCREMENT,
+    SupplierName TEXT NOT NULL,
+    ContactInfo TEXT UNIQUE
+)
+""")
